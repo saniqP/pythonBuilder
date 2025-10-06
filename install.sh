0308a4ed10e8cc586c1a8c58c74aa70a506146ca
blackHoleQp <undertalesyt@gmail.com>
2025-10-06 11:26:36 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ mkdir ~/.local/share/pybuild
 cp -r * ~/.local/share/pybuild
 
 echo "alias pybuild='python3 ~/.local/share/pybuild/builder.py'" >> ~/.zshrc
-echo "alias pybuild='python3 ~  /.local/share/pybuild/builder.py'" >> ~/.bashrc
+echo "alias pybuild='python3 ~/.local/share/pybuild/builder.py'" >> ~/.bashrc
