04308ec64de3443dec259b00389a81e6f1a008b7
blackHoleQp <undertalesyt@gmail.com>
2025-10-17 16:27:21 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir ~/.local/share/pybuild
-cp -r * ~/.local/share/pybuild
-
-echo "alias pybuild='python3 ~/.local/share/pybuild/builder.py'" >> ~/.zshrc
-echo "alias pybuild='python3 ~/.local/share/pybuild/builder.py'" >> ~/.bashrc
