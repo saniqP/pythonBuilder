f602f07213111e3303c310f165c64f10de43918b
saniqp <undertalesyt@gmail.com>
2025-11-17 14:28:56 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 mkdir ~/.local/share/pybuild
 cp -r * ~/.local/share/pybuild
 
-chmod +x /.local/share/pybuild/main
+chmod +x ~/.local/share/pybuild/main
 
 echo "alias pybuild='~/.local/share/pybuild/main'" >> ~/.zshrc
 echo "alias pybuild='~/.local/share/pybuild/main'" >> ~/.bashrc
